88026529073ebd7dde1d78cba4db8ec7965bb133
Nhật Nam <nguyentrungnhatnam@gmail.com>
2020-06-06 12:46:30 +0700

    [BE] - first commit
    make it better

---
+++ notification-manage/function/handler.js
@@ -0,0 +1,15 @@
+module.exports = async (config) => {
+	const app = config.app;
+	app.get("/", (req, res) => {
+		return res.status(200).send(req.body);
+	});
+	app.post("/", (req, res) => {
+		return res.status(200).send(req.body);
+	});
+	app.put("/", (req, res) => {
+		return res.status(200).send(req.body);
+	});
+	app.patch("/", (req, res) => {
+		return res.status(200).send(req.body);
+	});
+}
---
+++ notification-manage/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const handler = require("./function/handler")
+
+async function init() {
+	await handler({
+		app: app
+	})
+	app.listen(5000, (req, res)=> {
+		console.log(`Server is listening from PORT 5000`)
+	})
+}
